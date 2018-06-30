3606245600c882a5d2e8ac362a511e7526c6f822
Kai Strecker <do6kai@gmx.de>
2018-06-30 11:21:10 +0200

    Added basic init.el with use-package setup, gruvbox-theme and evil mode.

---
+++ init.el
@@ -0,0 +1,26 @@
+;; Add the melpa repo
+;; https://github.com/melpa/melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+;; use-package setup
+(eval-when-compile
+  (require 'use-package))
+
+(use-package gruvbox-theme
+  :config
+  (load-theme 'gruvbox-dark-soft t))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
